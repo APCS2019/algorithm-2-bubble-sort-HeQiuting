700920a0f48720c5e9db154a71377e6121806aac
HeQiuting <45375773+HeQiuting@users.noreply.github.com>
2018-12-18 10:00:14 +0800

    Update bubbleSort.java

--- bubbleSort.java
+++ bubbleSort.java
@@ -1,1 +1,12 @@
-
+int[] a = {5,3,1,6,9,3};
+int n = a.length;
+while(n > 0) {
+  for(int i = 0; i < n - 1; i++) {
+    if(a[i] > a[i+1]) {
+      int tmp = a[i];
+      a[i] = a[i+1];
+      a[i+1] = tmp;
+    }
+  }
+  n--;
+  }
